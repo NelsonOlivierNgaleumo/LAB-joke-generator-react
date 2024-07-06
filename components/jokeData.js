2375eff5ddc46283923ab527905b41d14d8ba8ef
Nelson Ngaleumo <nelsonngaleumo@yahoo.fr>
2024-07-06 12:04:25 -0500

    created Jokes.js and updated index.js file

---
+++ components/jokeData.js
@@ -0,0 +1,17 @@
+// import axios from 'axios';
+
+const endpoint = 'https://v2.jokeapi.dev/joke/Programming?safe-mode&type=twopart';
+
+const getJoke = () => new Promise((resolve, reject) => {
+  fetch(endpoint, {
+    method: 'GET',
+    header: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+export default getJoke;
